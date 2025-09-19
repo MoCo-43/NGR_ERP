d093627a148ba2020f88ef66395dfa2139d3f0af
Your Name <you@example.com>
2025-09-19 17:36:04 +0900

    folder hierarchy

--- src/main/java/com/yedam/erp/web/ApiController/VoucherController.java
+++ src/main/java/com/yedam/erp/web/ApiController/VoucherController.java
@@ -3,6 +3,7 @@ package com.yedam.erp.web.ApiController;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.yedam.erp.service.VoucherService;
 import com.yedam.erp.vo.VoucherVO;
@@ -15,7 +16,7 @@ import lombok.RequiredArgsConstructor;
 public class VoucherController {
 
     private final VoucherService service;
-
+    
     // ----------- 전체 조회 -----------
     @GetMapping
     public List<VoucherVO> list(@RequestParam(required = false) String type,
@@ -24,7 +25,7 @@ public class VoucherController {
         return service.list(type, status, keyword);
     }
 
-    // ----------- 단건 조회 -----------
+    // ----------- 단건 조회 (의문)-----------
     @GetMapping("/{id}")
     public VoucherVO get(@PathVariable Long id) {
         return service.get(id);
