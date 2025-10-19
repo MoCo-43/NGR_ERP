31f4a9b248001e0f28dafe5a4f9e579a38d87460
admin <pc@pc.com>
2025-10-19 22:49:42 +0900

    전자서명미리보기 다운로드,세금계산서 미리보기 다운로드 부분수정필요

--- src/main/java/com/yedam/erp/mapper/common/ApprovalMapper.java
+++ src/main/java/com/yedam/erp/mapper/common/ApprovalMapper.java
@@ -8,4 +8,7 @@ import org.apache.ibatis.annotations.Mapper;
 public interface ApprovalMapper {
 	// 전표 상태 + 전자결재
 	  int updateJournalStatus(Map<String, Object> param);
+	//구독 + 전자결제
+	 int updateDocumentSign(Map<String, Object> param);
+
 }
