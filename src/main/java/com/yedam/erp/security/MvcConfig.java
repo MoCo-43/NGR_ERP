f64dabc98aef532c9279e399e1cb01c7aec48d6a
Your Name <you@example.com>
2025-10-20 17:53:56 +0900

    "대쉬보드수정"

--- src/main/java/com/yedam/erp/security/MvcConfig.java
+++ src/main/java/com/yedam/erp/security/MvcConfig.java
@@ -24,12 +24,14 @@ public class MvcConfig implements WebMvcConfigurer {
 		       .addResourceLocations("file:" + uploadDir + "/sign/");
 	        
 	        // 공통
-
-	        
+	        registry.addResourceHandler("/uploads/signatures/**")
+            .addResourceLocations("file:" + uploadDir + "/uploads/signatures/");
 	        
 	    }
 
 	public void addViewControllers(ViewControllerRegistry registry) {
+		
+		
 		
 		// 메인
 		registry.addViewController("/").setViewName("main/homeMain");
